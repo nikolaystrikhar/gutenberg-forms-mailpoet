3981e475bbd23f1d08e2da140a918595dbfcbe00
Jack Kitterhing <codingmonkeyjack@gmail.com>
2022-05-18 23:14:53 +0100

    Update dev

--- init.php
+++ init.php
@@ -3,9 +3,9 @@
  * Plugin Name: Gutenberg Forms - MailPoet Add-on
  * Plugin URI: https://www.gutenbergforms.com
  * Description: This add-on connects Gutenberg Forms with MailPoet. This allows you to send leads/subscribers to your MailPoet list with the form submissions.
- * Author: munirkamal
- * Author URI: https://cakewp.com/
- * Version: 2.1.0
+ * Author: Tetra Soft
+ * Author URI: https://jak.dev
+ * Version: 2.1.1
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: cwp-gutenberg-forms-mailpoet
